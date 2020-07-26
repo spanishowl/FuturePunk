8a0964f58c3c89b4637ec95a2e92dc1f8277aafb
spanishowl <vivian@spanishowl.com>
2020-07-25 18:09:25 -0700

    Initial commit.

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    int fuelCapacity;
+    int speedCapacity;
+    List<Weapon> weapons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool IsDisabled()
+    {
+        return fuelCapacity == 0 || speedCapacity == 0;
+    }
+
+}
